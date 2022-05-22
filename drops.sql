f4be559c3e876cf1bd89f058649dd4e397b1c182
Naren Andres <105999404+baronrrojo@users.noreply.github.com>
2022-05-21 20:46:40 -0500

    Add files via upload

---
+++ drops.sql
@@ -0,0 +1,10 @@
+
+drop table requires;
+drop table teaches;
+drop table enrols;
+drop table course_offering;
+drop table course;
+drop table instructor;
+drop table student;
+drop FUNCTION create_teaches,mensaje_error,nota_igual,nota_no_valida,notificar_cambio;
+drop SEQUENCE student_id_seq,course_id_seq;
